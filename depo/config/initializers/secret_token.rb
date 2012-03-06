45f2e059a1ba4a06e9a6884559fc1723188a2154
Stefan Henseler <stefan.henseler@synax.ch>
2012-03-06 10:53:46 +0100

    First Commit

---
+++ depo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depo::Application.config.secret_token = 'a7492c30b249ab96ddcb182a7e4d4fab08bcbc3569f9995615033f95516e2742da6327ce33e279b90aacb715c35904eb940a6ac3a0beb3bbb96a737577dac4b4'
