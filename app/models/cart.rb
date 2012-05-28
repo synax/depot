489a39b45518f74e47cd65b475c677216954f323
Stefan Henseler <stefan.henseler@synax.ch>
2012-05-28 22:55:07 +0200

    Chapter 20

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,3 @@
-#---
 # Excerpted from "Agile Web Development with Rails",
 # published by The Pragmatic Bookshelf.
 # Copyrights apply to this code. It may not be used to create training material, 
@@ -8,4 +7,18 @@
 #---
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = line_items.find_by_product_id(product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
